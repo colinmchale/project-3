f2e7d20ae93260fca573624664c7dab4b7da33e5
Dylan Honeyman <dylan@dylans-iMac.local>
2022-02-04 12:58:37 -0600

    submittion form for new product on profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductList from '../components/ProductList';
 import { useQuery } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
+import { ADD_PRODUCT } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+
 
 const Profile = () => {
+    const [formState, setFormState] = useState({ name: '', description: '',  image:'', starting_price:'', category:'' });
     const { loading, data } = useQuery(QUERY_ME);
+    const [addProduct, { error }] = useMutation(ADD_PRODUCT);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            // const mutationResponse = 
+            return await addProduct({
+                variables: { name: formState.email, description: formState.password, image: formState.image, starting_price: formState.starting_price, category: formState.category },
+            });
+            // const token = mutationResponse.data.login.token;
+            // Auth.login(token);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+
     const products = data?.me.listings || [];
     console.log(products)
     return (
@@ -14,11 +43,49 @@ const Profile = () => {
                     {loading ? (
                         <div>Loading...</div>
                     ) : (
-                        <ProductList 
+                        <ProductList
                             products={products}
                             title="Some Food for Thoughts..." />
                     )}
                 </div>
+            </div>
+            <div className="row">
+                <h5>Create Product Listing</h5>
+                <form onSubmit={handleFormSubmit} className="col s12">
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input name="name" onChange={handleChange} type="text" className="validate" />
+                            <label for="name">Name</label>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s3">
+                                <input name='image' onChange={handleChange} type="text" className="validate" />
+                                <label for="Image">Image</label>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s3">
+                                    <input name='price' onChange={handleChange} id="price" type="text" className="validate" />
+                                    <label for="price">Price</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="input-field col s12">
+                            <input name='description' onChange={handleChange} id="description" type="text" className="validate" />
+                            <label for="description">Description</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <select name='category' onChange={handleChange} className="browser-default">
+                                <option value="" disabled selected>Choose Product Category</option>
+                                <option value="1">Option 1</option>
+                                <option value="2">Option 2</option>
+                                <option value="3">Option 3</option>
+                            </select>
+                            {/* <label>Materialize Select</label> */}
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     )
