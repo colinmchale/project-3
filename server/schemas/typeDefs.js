466fbc8c921bf8edc60229ad353a1ac16555c68d
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2022-02-04 13:03:44 -0600

    fixed addProduct mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -66,8 +66,8 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-    updateUser(firstName: String, lastName: String, email: String, password: String): User
-    addProduct(name: String!, description: String!, image: String, starting_price: Float!, current_price:Float, date_created: String, expiration_time: String): Product
+    addProduct(name:String!, description: String!, image: String, starting_price: Float!, current_price:Float, date_created:String, expiration_time:String, category:ID!): Product
+    updateUserListing(user:ID!, product:ID!): User
     updateProductPrice(_id: ID!, current_price: Float!): Product
     removeProduct(_id: ID!, expiration_time: String!): Product
     addBid(product_id:ID!, price:Float!): Bid
