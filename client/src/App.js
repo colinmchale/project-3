18f492fb991bc751744150dfb0fa2dab87c678ef
Dylan Honeyman <dylan@dylans-iMac.local>
2022-02-07 08:30:58 -0600

    getting heroku working

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ import SingleProduct from './pages/Product';
 import OrderHistory from './pages/OrderHistory';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
