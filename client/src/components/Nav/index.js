f8fd8e1fb296f7d173a7d51edec566d3c0cca375
Matt Reynolds <mattreynolds53@gmail.com>
2022-02-06 21:38:56 -0600

    Added additional styling to the navbar and profile pages.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,20 +3,15 @@ import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 
 function Nav() {
-
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/profile">
-              Profile
-            </Link>
+            <Link to="/profile">Profile</Link>
           </li>
           <li className="mx-1">
-            <Link to="/orderHistory">
-              Order History
-            </Link>
+            <Link to="/orderHistory">Order History</Link>
           </li>
           <li className="mx-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
@@ -30,14 +25,10 @@ function Nav() {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
+            <Link to="/signup">Signup</Link>
           </li>
           <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       );
@@ -50,35 +41,25 @@ function Nav() {
       justifyContent: "flex-start",
       alignItems: "center",
       // marginTop: "40px"
-
     },
     navBar: {
-      
-    }
-  }
+      display: "inline"
+    },
+  };
 
   return (
     <header className="flex-row px-1">
-      {/* <h1> */}
-      {/* </h1> */}
-
-      <nav className='right'>
-      {/* <nav> */}
-    <div className="nav-wrapper" style={styles.navBar}>
-        <Link className ='brand-logo valign-wrapper' to="/">
-          <img src='images/bid.png' />
-           Paddle
-           
-        </Link>
-      {/* <a href="#" class="brand-logo">Logo</a> */}
-      {/* <ul id="nav-mobile" class="right hide-on-med-and-down"> */}
-        {/* <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li> */}
-      {/* </ul> */}
-        <div className='right hide-on-med-and-down'>{showNavigation()}</div>
-    </div>
-  </nav>
+      <nav className="right">
+        <div className="nav-wrapper">
+          <Link className="brand-logo" to="/">
+            Paddle
+          </Link>
+          <div className="center"><img src="images/bid.png" /></div>
+          <div className="right hide-on-med-and-down" style={styles.navBar}>
+            {showNavigation()}
+          </div>
+        </div>
+      </nav>
       {/* </nav> */}
     </header>
   );
